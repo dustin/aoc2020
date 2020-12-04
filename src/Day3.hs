9fe6e6f15093febddd43ad73fccbc6c7cb53493b
Dustin Sallings <dustin@spy.net>
2020-12-03 20:26:01 -0800

    Return trees as a Sum Int which makes a simple fold of trees.

--- src/Day3.hs
+++ src/Day3.hs
@@ -2,6 +2,7 @@
 
 module Day3 where
 
+import           Data.Coerce     (coerce)
 import           Data.Foldable   (fold)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -16,15 +17,14 @@ getInput :: FilePath -> IO World
 getInput fn = parseGrid (\case '#' -> 1; _ -> 0) <$> readFile fn
 
 -- Count the trees along a given slope.
-trees :: World -> (Int, Int) -> Int
-trees m (xoff, yoff) = getSum . fold $ slope
+trees :: World -> (Int, Int) -> Sum Int
+trees m (xoff, yoff) = fold [m Map.! pos i | i <- [0 .. (maxy `div` yoff)]]
   where
     ((maxx, maxy),_) = Map.findMax m
     pos i = (i*xoff `mod` (maxx+1), i*yoff)
-    slope = [m Map.! pos i | i <- [0 .. (maxy `div` yoff)]]
 
 part1 :: World -> Int
-part1 w = trees w (3,1)
+part1 w = getSum $ trees w (3,1)
 
 part2 :: World -> Int
-part2 w = getProduct . foldMap (Product . trees w) $ [(1,1), (3,1), (5, 1), (7, 1), (1, 2)]
+part2 w = getProduct . foldMap (coerce . trees w) $ [(1,1), (3,1), (5, 1), (7, 1), (1, 2)]
