d252f3f0ab152c50a448b314b7d1a4c6fb392ba7
Dustin Sallings <dustin@spy.net>
2020-12-03 19:27:58 -0800

    Use Monoids and foldMap to do the things.
    I think this is similar to regular fusion, but I don't need to even
    have a conceptual list if all I'm doing is adding or multiplying
    items.
    
    I can store the trees as `Sum Int` since I only ever add trees, which
    is kind of neat.

--- src/Day3.hs
+++ src/Day3.hs
@@ -4,18 +4,19 @@ module Day3 where
 
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import           Data.Monoid     (Product (..), Sum (..))
 
 import           Advent.AoC
 import           Advent.TwoD
 
-type World = Map Point Int
+type World = Map Point (Sum Int)
 
 getInput :: FilePath -> IO World
 getInput fn = parseGrid (\case '#' -> 1; _ -> 0) <$> readFile fn
 
 -- Count the trees along a given slope.
 trees :: World -> (Int, Int) -> Int
-trees m (xoff,yoff) = sum . fmap at $ slope
+trees m (xoff,yoff) = getSum . foldMap at $ slope
   where
     ((maxx, maxy),_) = Map.findMax m
     at (x,y) = m Map.! (x `mod` (maxx+1), y)
@@ -25,4 +26,4 @@ part1 :: World -> Int
 part1 w = trees w (3,1)
 
 part2 :: World -> Int
-part2 w = product . fmap (trees w) $ [(1,1), (3,1), (5, 1), (7, 1), (1, 2)]
+part2 w = getProduct . foldMap (Product . trees w) $ [(1,1), (3,1), (5, 1), (7, 1), (1, 2)]
