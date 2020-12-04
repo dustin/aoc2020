fd9e1d0131f1272542f71c662194a94ae6c92ac6
Dustin Sallings <dustin@spy.net>
2020-12-03 20:14:42 -0800

    Generate only the in-bounds positions on the slope.
    Creating an infinite list along the slope and then filtering it down
    was the initially obvious thing to do, but it's easy enough to just
    generate the list once in the list comprehension, then I can use a
    plain fold to compute the Sum.

--- src/Day3.hs
+++ src/Day3.hs
@@ -2,6 +2,7 @@
 
 module Day3 where
 
+import           Data.Foldable   (fold)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import           Data.Monoid     (Product (..), Sum (..))
@@ -16,11 +17,11 @@ getInput fn = parseGrid (\case '#' -> 1; _ -> 0) <$> readFile fn
 
 -- Count the trees along a given slope.
 trees :: World -> (Int, Int) -> Int
-trees m (xoff,yoff) = getSum . foldMap at $ slope
+trees m (xoff, yoff) = getSum . fold $ slope
   where
     ((maxx, maxy),_) = Map.findMax m
-    at (x,y) = m Map.! (x `mod` (maxx+1), y)
-    slope = takeWhile ((<= maxy) . snd) [(i*xoff, i*yoff) | i <- [0..]]
+    pos i = (i*xoff `mod` (maxx+1), i*yoff)
+    slope = [m Map.! pos i | i <- [0 .. (maxy `div` yoff)]]
 
 part1 :: World -> Int
 part1 w = trees w (3,1)
