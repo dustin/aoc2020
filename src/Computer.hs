4e87dd7789eaed5c01ffb6082718b61e79e6ff1e
Dustin Sallings <dustin@spy.net>
2020-12-08 09:18:13 -0800

    Slight changes/docs for Computer

--- src/Computer.hs
+++ src/Computer.hs
@@ -26,23 +26,32 @@ parseInstr = Instruction <$> lexeme op <*> L.signed space L.decimal <* "\n"
          <|> ACC <$ "acc"
          <|> JMP <$ "jmp"
 
+-- | readProgram parses a file of instructions into a Program.
 readProgram :: FilePath -> IO Program
 readProgram = fmap V.fromList . parseFile (some parseInstr)
 
-evalStep :: Program -> Int -> Int -> (Int, Int)
-evalStep prog pc acc = case prog V.! pc of
-                         Instruction NOOP _ -> (pc + 1, acc)
-                         Instruction ACC x  -> (pc + 1, acc + x)
-                         Instruction JMP x  -> (pc + x, acc)
+-- | Program state is the PC and Accumulator value
+type ProgramState = (Int, Int)
 
-run :: Program -> [(Int, Int)]
-run prog = iterate (uncurry (evalStep prog)) (0, 0)
+-- | evalStep executes a single instruction from a given state and returns the new state.
+evalStep :: Program -> ProgramState -> ProgramState
+evalStep prog (pc, acc) = case prog V.! pc of
+                            Instruction NOOP _ -> (pc + 1, acc)
+                            Instruction ACC x  -> (pc + 1, acc + x)
+                            Instruction JMP x  -> (pc + x, acc)
 
-loopOrTerminate :: Program -> Either (Int,Int) Int
+-- | Run returns a continuous stream of program states from an initial state.
+run :: Program -> [ProgramState]
+run prog = iterate (evalStep prog) (0, 0)
+
+-- | loopOrTerminate returns either a Left program state at a the
+-- point *before* a loop occurs, or the Right final accumulator state.
+loopOrTerminate :: Program -> Either ProgramState Int
 loopOrTerminate program = f mempty . run $ program
   where
+    end = length program
     f _ ((n, x):_)
-      | n == length program = Right x
+      | n == end = Right x
     f s ((opc,_):n@(npc, x):xs)
       | npc `Set.member` s = Left (opc, x)
       | otherwise = f (Set.insert npc s) (n:xs)
