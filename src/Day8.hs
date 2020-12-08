7339445a03f90cd9644d06841e8ecd7cd9de651e
Dustin Sallings <dustin@spy.net>
2020-12-07 22:42:15 -0800

    Make day 8 total

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,24 +1,26 @@
 module Day8 where
 
-import           Data.Either (fromLeft, rights)
-import qualified Data.Vector as V
+import           Data.Bifunctor (first)
+import           Data.Either    (rights)
+import           Data.Maybe     (listToMaybe)
+import qualified Data.Vector    as V
 
 import           Computer
-
-part1 :: Program -> Int
-part1 = snd . fromLeft undefined . loopOrTerminate
 
 getInput :: FilePath -> IO Program
 getInput = readProgram
 
-bruteforce :: Program -> [Program]
-bruteforce = fmap V.fromList . go . V.toList
-  where
-    go :: [Instruction] -> [[Instruction]]
-    go []                          = []
-    go (i@(Instruction NOOP x):xs) = [Instruction JMP x:xs] <> fmap (i:) (go xs)
-    go (i@(Instruction JMP x):xs)  = [Instruction NOOP x:xs] <> fmap (i:) (go xs)
-    go (x:xs)                      = fmap (x:) (go xs)
+part1 :: Program -> Either Int Int
+part1 = first snd . loopOrTerminate
 
-part2 :: Program -> Int
-part2 = head . rights . fmap loopOrTerminate . bruteforce
+part2 :: Program -> Maybe Int
+part2 = listToMaybe . rights . fmap loopOrTerminate . bruteforce
+  where
+    bruteforce :: Program -> [Program]
+    bruteforce = fmap V.fromList . go . V.toList
+      where
+        go :: [Instruction] -> [[Instruction]]
+        go []                          = []
+        go (i@(Instruction NOOP x):xs) = [Instruction JMP x:xs] <> fmap (i:) (go xs)
+        go (i@(Instruction JMP x):xs)  = [Instruction NOOP x:xs] <> fmap (i:) (go xs)
+        go (x:xs)                      = fmap (x:) (go xs)
--- test/Day8Tests.hs
+++ test/Day8Tests.hs
@@ -9,10 +9,10 @@ import           Test.Tasty.QuickCheck as QC
 import           Day8
 
 testPart1 :: Assertion
-testPart1 = assertEqual "" 1489 . part1 =<< getInput "input/day8"
+testPart1 = assertEqual "" (Left 1489) . part1 =<< getInput "input/day8"
 
 testPart2 :: Assertion
-testPart2 = assertEqual "" 1539 . part2 =<< getInput "input/day8"
+testPart2 = assertEqual "" (Just 1539) . part2 =<< getInput "input/day8"
 
 tests :: [TestTree]
 tests = [
