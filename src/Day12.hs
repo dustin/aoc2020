40d848101a8f3f20f0bf01d8a7b0c8873774b634
Dustin Sallings <dustin@spy.net>
2020-12-11 23:15:57 -0800

    Lensed up my day 12 a bit

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE DeriveGeneric      #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell    #-}
 {-# Options_GHC -Wno-orphans #-}
 
 module Day12 where
 
 import           Control.Applicative        ((<|>))
 import           Control.DeepSeq            (NFData (..), rwhnf)
+import           Control.Lens
 import           Data.Foldable              (foldl')
 import           Text.Megaparsec            (endBy)
 import           Text.Megaparsec.Char.Lexer (decimal)
@@ -24,18 +26,20 @@ instance NFData Dir where rnf = rwhnf
 instance NFData Action where rnf = rwhnf
 
 data Ship = Ship {
-  shipDir    :: Dir
-  , shipPos  :: Point
-  , wayPoint :: Point
+  _shipDir    :: Dir
+  , _shipPos  :: Point
+  , _wayPoint :: Point
   } deriving Show
+
+makeLenses ''Ship
 
 parseAction :: Parser Action
 parseAction = parseDir <|> parseLeft <|> parseRight <|> parseForward
   where
     parseDir = MoveDir <$> parseD <*> decimal
     parseD = N <$ "N" <|> E <$ "E" <|> S <$ "S" <|> W <$ "W"
-    parseLeft = TurnLeft <$> ("L" *> decimal)
-    parseRight = TurnRight <$> ("R" *> decimal)
+    parseLeft = TurnLeft <$> ("L" *> fmap (`div` 90) decimal)
+    parseRight = TurnRight <$> ("R" *> fmap (`div` 90) decimal)
     parseForward = Forward <$> ("F" *> decimal)
 
 getInput :: FilePath -> IO [Action]
@@ -44,24 +48,24 @@ getInput = parseFile (parseAction `endBy` "\n")
 ntimes :: Int -> (a -> a) -> a -> a
 ntimes n f a = iterate f a !! n
 
-doit :: (Ship -> Action -> Ship) -> [Action] -> Int
-doit f = mdist2 (0,0) . shipPos . foldl' f (Ship E (0,0) (10,1))
+doit :: (Action -> Ship -> Ship) -> [Action] -> Int
+doit f = mdist2 (0,0) . _shipPos . foldl' (flip f) (Ship E (0,0) (10,1))
 
 part1 :: [Action] -> Int
 part1 = doit apply
   where
-    apply ship@Ship{..} (MoveDir dir amt) = ship{shipPos = fwdBy amt dir shipPos}
-    apply ship@Ship{..} (Forward amt)     = ship{shipPos = fwdBy amt shipDir shipPos}
-    apply ship@Ship{..} (TurnLeft n)      = ship{shipDir = (ntimes (n `div` 90) pred') shipDir}
-    apply ship@Ship{..} (TurnRight n)     = ship{shipDir = (ntimes (n `div` 90) succ') shipDir}
+    apply (MoveDir dir amt) = shipPos %~ fwdBy amt dir
+    apply (Forward amt)     = \s@Ship{_shipDir} -> s & shipPos %~ fwdBy amt _shipDir
+    apply (TurnLeft n)      = shipDir %~ ntimes n pred'
+    apply (TurnRight n)     = shipDir %~ ntimes n succ'
 
 part2 :: [Action] -> Int
 part2 = doit apply
   where
-    apply ship@Ship{..} (MoveDir dir amt) = ship{wayPoint = fwdBy amt dir wayPoint}
-    apply ship@Ship{..} (Forward amt)     = ship{shipPos = addPoint (mulPoint (amt, amt) wayPoint) shipPos}
-    apply ship@Ship{..} (TurnLeft n)      = ship{wayPoint = (ntimes (n `div` 90) rotateLeft) wayPoint}
-    apply ship@Ship{..} (TurnRight n)     = ship{wayPoint = (ntimes (n `div` 90) rotateRight) wayPoint}
+    apply (MoveDir dir amt) = wayPoint %~ fwdBy amt dir
+    apply (Forward amt)     = \s@Ship{_wayPoint} -> s & shipPos  %~ (addPoint . mulPoint (amt, amt)) _wayPoint
+    apply (TurnLeft n)      = wayPoint %~ ntimes n rotateLeft
+    apply (TurnRight n)     = wayPoint %~ ntimes n rotateRight
 
     rotateLeft (x,y) = (-y, x)
     rotateRight (x,y) = (y, -x)
