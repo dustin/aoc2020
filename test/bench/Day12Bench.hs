e8ed92b17b65df61222aa97a915fb60a1ed5f63b
Dustin Sallings <dustin@spy.net>
2020-12-12 00:10:51 -0800

    Don't need a separate function for the parse benchmark

--- test/bench/Day12Bench.hs
+++ test/bench/Day12Bench.hs
@@ -2,19 +2,15 @@ module Day12Bench where
 
 import           Criterion          (Benchmark, bench, bgroup, env, nf)
 import qualified Data.Array.Unboxed as UA
-import           Data.Text          (Text)
 import qualified Data.Text.IO       as TIO
 
 import           Advent.AoC
 
 import           Day12
 
-parseTest :: Text -> [Action]
-parseTest = parseLit parseActions
-
 tests :: [Benchmark]
 tests = [
-  env (TIO.readFile "input/day12") $ \x -> bench "parsing" $ nf parseTest x,
+  env (TIO.readFile "input/day12") $ \x -> bench "parsing" $ nf (parseLit parseActions) x,
   env (getInput "input/day12") $ \ ~x -> bgroup "" [
       bench "part1" $ nf part1 x,
       bench "part2" $ nf part2 x
