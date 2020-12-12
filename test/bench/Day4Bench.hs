749fbf73868e0a1a35e24337da0a284bfebc250e
Dustin Sallings <dustin@spy.net>
2020-12-11 23:32:48 -0800

    Benchmark day 4 parser

--- test/bench/Day4Bench.hs
+++ test/bench/Day4Bench.hs
@@ -1,11 +1,14 @@
 module Day4Bench where
 
-import           Criterion (Benchmark, bench, bgroup, env, nf)
+import           Criterion    (Benchmark, bench, bgroup, env, nf)
+import qualified Data.Text.IO as TIO
 
+import           Advent.AoC
 import           Day4
 
 tests :: [Benchmark]
 tests = [
+  env (TIO.readFile "input/day4") $ \x -> bench "parsing" $ nf (parseLit parsePassport) x,
   env (getInput "input/day4") $ \ ~x -> bgroup "" [
       bench "part1" $ nf part1 x,
       bench "part2" $ nf part2 x
