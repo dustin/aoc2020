ac8b1a485ccda972bd34bb5dab45195b729b2d15
Dustin Sallings <dustin@spy.net>
2020-12-13 10:15:52 -0800

    Move identification into the parser

--- src/Day13.hs
+++ src/Day13.hs
@@ -7,6 +7,7 @@ module Day13 where
 import           Control.Applicative              ((<|>))
 import           Control.DeepSeq                  (NFData (..), rwhnf)
 import           Data.Maybe                       (mapMaybe)
+import           Data.Tuple                       (swap)
 import           Math.NumberTheory.Moduli.Chinese
 import           Text.Megaparsec                  (sepBy)
 import           Text.Megaparsec.Char.Lexer       (decimal)
@@ -16,37 +17,39 @@ import           Advent.AoC
 
 data Input n = Input {
   _earliest :: n
-  , _buses  :: [Maybe n]
+  , _buses  :: [(n, n)]
   } deriving Show
 
 instance NFData (Input a) where rnf = rwhnf
 
-parseInput :: Num n => Parser (Input n)
-parseInput = Input <$> decimal <* "\n" <*> bus `sepBy` ","
-  where bus = Nothing <$ "x" <|> Just <$> decimal
+parseInput :: (Enum n, Num n) => Parser (Input n)
+parseInput = do
+  _earliest <- decimal <* "\n"
+  buses <- bus `sepBy` ","
+  let _buses = mapMaybe f $ zip buses [0..]
+  pure Input{..}
+  where
+    bus = Nothing <$ "x" <|> Just <$> decimal
+    f (Nothing, _) = Nothing
+    f (Just o, x)  = Just (o, x)
 
-getInput :: Num n => FilePath -> IO (Input n)
+getInput :: (Enum n, Num n) => FilePath -> IO (Input n)
 getInput = parseFile parseInput
 
 part1 :: Input Int -> Int
 part1 Input{..} = bid * waiting
   where
-    next i = (i - (_earliest `rem` i), i)
-    (waiting, bid) = minimum $ mapMaybe (fmap next) _buses
+    next (i,_) = (i - (_earliest `rem` i), i)
+    (waiting, bid) = minimum $ fmap next _buses
 
 part2 :: Input Integer -> Maybe Integer
-part2 Input{..} = chineseRemainder bs
-  where
-    bs = mapMaybe f (zip _buses [0..])
-    f (Just x, n) = Just (x-n `mod` x, x)
-    f _           = Nothing
+part2 Input{..} = chineseRemainder (fmap (\(x, n) -> (x-n `mod` x, x)) _buses)
 
 -- nshepperd
 part2ns :: Input Integer -> Integer
-part2ns Input{..} = go rules [0..]
+part2ns Input{..} = go (fmap swap _buses) [0..]
   where
     go [] ts     = head ts
     go (r:rs) ts = go rs (accelerate (filter (solves r) ts))
     solves (dt,n) t = mod (t+dt) n == 0
     accelerate ts = let a:b:_ = ts in [a,b..]
-    rules = [(dt, n) | (dt, Just n) <- zip [0..] _buses]
--- test/bench/Day13Bench.hs
+++ test/bench/Day13Bench.hs
@@ -2,11 +2,15 @@ module Day13Bench where
 
 import           Control.DeepSeq (NFData (..), rwhnf)
 import           Criterion       (Benchmark, bench, bgroup, env, nf)
+import qualified Data.Text.IO    as TIO
+
+import           Advent.AoC
 
 import           Day13
 
 tests :: [Benchmark]
 tests = [
+  env (TIO.readFile "input/day13") $ \x -> bench "parsing" $ nf (parseLit parseInput) x,
   env (getInput "input/day13") $ \ ~x -> bgroup "int" [
       bench "part1" $ nf part1 x
       ],
