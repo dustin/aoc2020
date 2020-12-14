ed6f119122e11d7d620b8a83b3c31e5dae2a8f7f
Dustin Sallings <dustin@spy.net>
2020-12-14 13:56:39 -0800

    Essentializing a bit as a meeting doodle.

--- src/Day14.hs
+++ src/Day14.hs
@@ -7,7 +7,6 @@ import           Control.Applicative        (liftA2, (<|>))
 import           Control.DeepSeq            (NFData (..), rwhnf)
 import           Data.Bits                  (complement, (.&.), (.|.))
 import           Data.Foldable              (foldl')
-import           Data.IntMap.Strict         (IntMap)
 import qualified Data.IntMap.Strict         as IntMap
 import           Text.Megaparsec            (endBy, satisfy, some)
 import           Text.Megaparsec.Char.Lexer (decimal)
@@ -16,12 +15,10 @@ import           Advent.AoC
 
 type Program = [Manipulation]
 
-type WordType = Int
-
-type BitMask = (WordType, WordType)
+type BitMask = (Int, Int)
 
 data Manipulation = Mask !BitMask ![BitMask] -- zero mask, one mask, xmasks
-                  | Mem !WordType !WordType deriving Show
+                  | Mem !Int !Int deriving Show
 
 instance NFData Manipulation where rnf = rwhnf
 
@@ -29,40 +26,34 @@ getInput :: FilePath -> IO Program
 getInput = parseFile parseProgram
 
 parseProgram :: Parser Program
-parseProgram = seg `endBy` "\n"
-  where
-    seg = parseMem <|> parseMask
-
-parseMem :: Parser Manipulation
-parseMem = liftA2 Mem ("mem[" *> decimal <* "] = ") decimal
-
-parseMask :: Parser Manipulation
-parseMask = do
-  _ <- "mask = "
-  rawMask <- some (satisfy (`elem` ['X', '0', '1']))
-  let (z,o) = masks rawMask
-      xes = masks <$> traverse (\case 'X' -> "01"; '0' -> "."; x -> [x]) rawMask
-  pure $ Mask (z, o) xes
+parseProgram = (parseMem <|> parseMask) `endBy` "\n"
 
   where
-    masks r = (complement (mkmask '0' r), mkmask '1' r)
-    mkmask c = foldl' (\o' x -> o' * 2 + if x == c then 1 else 0) 0
+    parseMem = liftA2 Mem ("mem[" *> decimal <* "] = ") decimal
 
-applyMask :: BitMask -> WordType -> WordType
+    parseMask = do
+      rawMask <- "mask = " *> some (satisfy (`elem` ['X', '0', '1']))
+      let (z,o) = masks rawMask
+          xes = masks <$> traverse (\case 'X' -> "01"; '0' -> "."; x -> [x]) rawMask
+      pure $ Mask (z, o) xes
+
+        where
+          masks r = (complement (mkmask '0' r), mkmask '1' r)
+          mkmask c = foldl' (\o' x -> o' * 2 + if x == c then 1 else 0) 0
+
+applyMask :: BitMask -> Int -> Int
 applyMask !(!z, !o) w = w .&. z .|. o
 
-part1 :: Program -> WordType
+part1 :: Program -> Int
 part1 = sum . run mempty (0, 0)
   where
-    run :: IntMap WordType -> BitMask -> [Manipulation] -> IntMap WordType
     run s _ []              = s
     run s _ (Mask m _:xs)   = run s m xs
     run s mask (Mem i w:xs) = run (IntMap.insert i (applyMask mask w) s) mask xs
 
-part2 :: Program -> WordType
+part2 :: Program -> Int
 part2 = sum . run mempty mempty
   where
-    run :: IntMap WordType -> [BitMask] -> [Manipulation] -> IntMap WordType
     run s _ []               = s
     run s _ (Mask _ ms:xs)   = run s ms xs
-    run s masks (Mem i w:xs) = run (foldMap (\m -> IntMap.singleton (applyMask m i) w) masks <> s) masks xs
+    run s masks (Mem i w:xs) = run (IntMap.fromList [(applyMask m i, w) | m <- masks] <> s) masks xs
