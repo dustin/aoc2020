e977738251ca80a0cf2fffd9a437085c32d19fbc
Dustin Sallings <dustin@spy.net>
2020-12-14 09:31:18 -0800

    Use IntMap for Day 14

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase   #-}
 
 module Day14 where
 
@@ -6,9 +7,8 @@ import           Control.Applicative        (liftA2, (<|>))
 import           Control.DeepSeq            (NFData (..), rwhnf)
 import           Data.Bits                  (complement, (.&.), (.|.))
 import           Data.Foldable              (foldl')
-import           Data.Map.Strict            (Map)
-import qualified Data.Map.Strict            as Map
-import           Data.Word                  (Word64)
+import           Data.IntMap.Strict         (IntMap)
+import qualified Data.IntMap.Strict         as IntMap
 import           Text.Megaparsec            (endBy, satisfy, some)
 import           Text.Megaparsec.Char.Lexer (decimal)
 
@@ -16,10 +16,12 @@ import           Advent.AoC
 
 type Program = [Manipulation]
 
-type BitMask = (Word64, Word64)
+type WordType = Int
 
-data Manipulation = Mask BitMask [BitMask] -- zero mask, one mask, xmasks
-                  | Mem Word64 Word64 deriving Show
+type BitMask = (WordType, WordType)
+
+data Manipulation = Mask !BitMask ![BitMask] -- zero mask, one mask, xmasks
+                  | Mem !WordType !WordType deriving Show
 
 instance NFData Manipulation where rnf = rwhnf
 
@@ -43,25 +45,24 @@ parseMask = do
   pure $ Mask (z, o) xes
 
   where
-    masks r = (zmask r, omask r)
-    zmask = foldl' (\o' x -> o' * 2 + if x == '0' then 1 else 0) 0
-    omask = foldl' (\o' x -> o' * 2 + if x == '1' then 1 else 0) 0
+    masks r = (mkmask '0' r, mkmask '1' r)
+    mkmask c = foldl' (\o' x -> o' * 2 + if x == c then 1 else 0) 0
 
-applyMask :: BitMask -> Word64 -> Word64
-applyMask (z, o) w = w .&. complement z .|. o
+applyMask :: BitMask -> WordType -> WordType
+applyMask !(!z, !o) w = w .&. complement z .|. o
 
-part1 :: Program -> Word64
+part1 :: Program -> WordType
 part1 = sum . run mempty (0, 0)
   where
-    run :: Map Word64 Word64 -> BitMask -> [Manipulation] -> Map Word64 Word64
+    run :: IntMap WordType -> BitMask -> [Manipulation] -> IntMap WordType
     run s _ []              = s
     run s _ (Mask m _:xs)   = run s m xs
-    run s mask (Mem i w:xs) = run (Map.insert i (applyMask mask w) s) mask xs
+    run s mask (Mem i w:xs) = run (IntMap.insert i (applyMask mask w) s) mask xs
 
-part2 :: Program -> Word64
+part2 :: Program -> WordType
 part2 = sum . run mempty mempty
   where
-    run :: Map Word64 Word64 -> [BitMask] -> [Manipulation] -> Map Word64 Word64
+    run :: IntMap WordType -> [BitMask] -> [Manipulation] -> IntMap WordType
     run s _ []               = s
     run s _ (Mask _ ms:xs)   = run s ms xs
-    run s masks (Mem i w:xs) = run (foldr (\x o -> Map.insert (applyMask x i) w o) s masks) masks xs
+    run s masks (Mem i w:xs) = run (foldr (\x o -> IntMap.insert (applyMask x i) w o) s masks) masks xs
