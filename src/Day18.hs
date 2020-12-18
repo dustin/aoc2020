2ae83c5e47910f8b0bdf49727a9d6cc71a3bcb01
Dustin Sallings <dustin@spy.net>
2020-12-17 22:56:52 -0800

    Sloshing code around mindlessly

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GADTs #-}
+
 module Day18 where
 
 import           Control.Applicative            ((<|>))
@@ -12,8 +14,9 @@ import qualified Text.Megaparsec.Char.Lexer     as L
 
 import           Advent.AoC
 
-data Expression n = EVal n
-                  | EFun (String, n -> n -> n) [Expression n]
+data Expression n where
+  EVal :: n -> Expression n
+  EFun :: (String, n -> n -> n) -> [Expression n] -> Expression n
 
 instance NFData (Expression n) where rnf = rwhnf
 
@@ -21,9 +24,11 @@ type Ops n = [[Operator Parser (Expression n)]]
 
 instance Show n => Show (Expression n) where
   show (EVal x) = show x
-  show (EFun (fn, _) exps) = intercalate fn (map inner exps)
-    where inner (EVal x)             = (show) x
-          inner (EFun (fn',_) exps') = "(" <> intercalate fn' (map inner exps') <> ")"
+  show e@(EFun _ _) = sx e
+    where inner (EVal x)      = show x
+          inner e'@(EFun _ _) = "(" <> sx e' <> ")"
+          sx (EVal x)           = show x
+          sx (EFun (fn,_) exps) = intercalate fn (map inner exps)
 
 parseExpr :: Num n => [[Operator Parser (Expression n)]] -> Parser (Expression n)
 parseExpr ops = makeExprParser term ops
@@ -31,17 +36,17 @@ parseExpr ops = makeExprParser term ops
         parens = lexeme . between "(" ")"
         lexeme = L.lexeme hspace
 
-op :: String -> (n -> n -> n) -> Operator Parser (Expression n)
-op s f = InfixL (binify (EFun (s, f)) <$ L.symbol hspace (pack s))
-  where
-    binify :: ([Expression n] -> Expression n) -> Expression n -> Expression n -> Expression n
-    binify e a b =  e [a, b]
-
 flat :: Num n => Ops n
 flat = [[op "+" (+), op "*" (*)]]
+  where
+    op :: String -> (n -> n -> n) -> Operator Parser (Expression n)
+    op s f = InfixL (binify (EFun (s, f)) <$ L.symbol hspace (pack s))
+      where
+        binify :: ([Expression n] -> Expression n) -> Expression n -> Expression n -> Expression n
+        binify e a b =  e [a, b]
 
 plusFirst :: Num n => Ops n
-plusFirst = [[op "+" (+)], [op "*" (*)]]
+plusFirst = sequenceA flat
 
 parseExprs :: Num n => Ops n -> Parser [Expression n]
 parseExprs o = parseExpr o `endBy` "\n"
@@ -50,10 +55,8 @@ getInput :: Num n => Ops n -> FilePath -> IO [Expression n]
 getInput o = parseFile (parseExprs o)
 
 evalExpr :: Num n => Expression n -> n
-evalExpr = go
-  where
-    go (EVal v)          = v
-    go (EFun (_,f) exps) = foldr1 f (map go exps)
+evalExpr (EVal v)          = v
+evalExpr (EFun (_,f) exps) = foldr1 f (map evalExpr exps)
 
 part1 :: [Expression (Sum Int)] -> Int
 part1 = getSum . foldMap evalExpr
