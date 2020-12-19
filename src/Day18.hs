50255ece405cf2b4d89cc4b406a630cce52da6d2
Dustin Sallings <dustin@spy.net>
2020-12-18 19:22:21 -0800

    Expression is Foldable/Monoidal.
    Removed all the things that didn't contribute to an answer.

--- src/Day18.hs
+++ src/Day18.hs
@@ -5,6 +5,7 @@ module Day18 where
 import           Control.Applicative            ((<|>))
 import           Control.DeepSeq                (NFData (..), rwhnf)
 import           Control.Monad.Combinators.Expr (Operator (..), makeExprParser)
+import           Data.Foldable                  (fold)
 import           Data.Semigroup                 (Sum (..))
 import           Text.Megaparsec                (between, endBy)
 import           Text.Megaparsec.Char           (hspace)
@@ -14,7 +15,19 @@ import           Advent.AoC
 
 data Expression n where
   EVal :: !n -> Expression n
-  EFun :: !(n -> n -> n) -> !(Expression n, Expression n) -> Expression n
+  EFun :: !(n -> n -> n) -> !(Expression n) -> !(Expression n) -> Expression n
+
+instance Semigroup n => Semigroup (Expression n) where
+  a <> b = EFun (<>) a b
+
+instance Monoid n => Monoid (Expression n) where
+  mempty = EVal mempty
+
+instance Foldable Expression where
+  foldMap f (EVal x) = f x
+  foldMap f e@(EFun _ _ _) = f (eval e)
+    where eval (EVal x)     = x
+          eval (EFun o a b) = eval a `o` eval b
 
 instance NFData (Expression n) where rnf = rwhnf
 
@@ -22,15 +35,11 @@ type Ops n = [[Operator Parser (Expression n)]]
 
 parseExpr :: Num n => Ops n -> Parser (Expression n)
 parseExpr ops = makeExprParser term ops
-  where term = parens (parseExpr ops) <|> EVal <$> lexeme L.decimal
-        parens = lexeme . between "(" ")"
-        lexeme = L.lexeme hspace
+  where term = L.lexeme hspace (between "(" ")" (parseExpr ops))
+               <|> EVal <$> L.lexeme hspace L.decimal
 
 flat :: Num n => Ops n
-flat = [[op "+" (+), op "*" (*)]]
-  where
-    op s f = InfixL (binify (EFun f) <$ L.symbol hspace s)
-    binify e a b =  e (a, b)
+flat = (fmap.fmap) (\(s,f) -> InfixL (EFun f <$ L.symbol hspace s)) [[("+",(+)), ("*", (*))]]
 
 plusFirst :: Num n => Ops n
 plusFirst = sequenceA flat
@@ -38,15 +47,14 @@ plusFirst = sequenceA flat
 parseExprs :: Num n => Ops n -> Parser [Expression n]
 parseExprs o = parseExpr o `endBy` "\n"
 
-evalExpr :: Num n => Expression n -> n
-evalExpr (EVal v)        = v
-evalExpr (EFun f (a, b)) = evalExpr a `f` evalExpr b
-
 getInput :: Num n => Ops n -> FilePath -> IO [Expression n]
 getInput o = parseFile (parseExprs o)
 
-part1 :: [Expression (Sum Int)] -> Int
-part1 = getSum . foldMap evalExpr
+ans :: (Num n, Monoid n) => [Expression n] -> n
+ans = fold . fold
 
-part2 :: [Expression (Sum Int)] -> Int
-part2 = getSum . foldMap evalExpr
+part1 :: [Expression (Sum Int)] -> Sum Int
+part1 = ans
+
+part2 :: [Expression (Sum Int)] -> Sum Int
+part2 = ans
