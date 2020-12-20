58a98d8072070c5cf87b0ceb43eba7bca8d26f23
Dustin Sallings <dustin@spy.net>
2020-12-19 17:59:08 -0800

    I don't need separate forms for sequences and ors.

--- src/Day19.hs
+++ src/Day19.hs
@@ -2,12 +2,13 @@ module Day19 where
 
 import           Control.Applicative          (liftA2, (<|>))
 import           Control.DeepSeq              (NFData (..), rwhnf)
+import           Data.Foldable                (asum)
 import           Data.Functor                 (($>))
 import           Data.List                    (foldl1')
 import           Data.Map.Strict              (Map)
 import qualified Data.Map.Strict              as Map
 import           Data.Text                    (Text, pack, unpack)
-import           Text.Megaparsec              (anySingle, endBy, many, option, satisfy, some)
+import           Text.Megaparsec              (anySingle, endBy, many, satisfy, sepBy, some)
 import           Text.Megaparsec.Char         (hspace, space)
 import qualified Text.Megaparsec.Char.Lexer   as L
 import qualified Text.ParserCombinators.ReadP as R
@@ -15,8 +16,7 @@ import qualified Text.ParserCombinators.ReadP as R
 import           Advent.AoC
 import           Advent.Search
 
-data Rule = Rule [Int]
-          | RuleOr [Int] [Int]
+data Rule = Rule [[Int]]
           | RuleLit Char
   deriving Show
 
@@ -28,13 +28,7 @@ parseRule :: Parser (Int, Rule)
 parseRule = liftA2 (,) (L.decimal <* lexeme ":") (parseChar <|> parseSeq)
   where
     parseChar = RuleLit <$> ("\"" *> anySingle <* "\"")
-    parseSeq = do
-      rs <- nums
-      alt <- option [] (lexeme "|" *> nums)
-      pure $ case alt of
-               [] -> Rule rs
-               oz -> RuleOr rs oz
-
+    parseSeq = Rule <$> nums `sepBy` lexeme "|"
     nums = many (lexeme L.decimal)
     lexeme = L.lexeme hspace
 
@@ -47,9 +41,8 @@ parseInput = do
 mkParser :: RuleMap -> Rule -> R.ReadP ()
 mkParser rm = go
   where
-    go (RuleLit c)  = R.satisfy (== c) $> ()
-    go (RuleOr l r) = p1 l <|> p1 r
-    go (Rule l)     = p1 l
+    go (RuleLit c) = R.satisfy (== c) $> ()
+    go (Rule ls)   = asum (p1 <$> ls)
     p1 ls = foldl1' (*>) (go . (rm Map.!) <$> ls)
 
 getInput :: FilePath -> IO (RuleMap, [Text])
@@ -64,6 +57,6 @@ part1 (rm, ins) = countIf rp ins
 part2 :: (RuleMap, [Text]) -> Int
 part2 (rm, ins) = part1 (rm', ins)
   where
-    rm' = (Map.fromList [(8, RuleOr [42] [42, 8]),
-                         (11, RuleOr [42, 31] [42, 11, 31])]) <> rm
+    rm' = (Map.fromList [(8, Rule [[42], [42, 8]]),
+                         (11, Rule [[42, 31], [42, 11, 31]])]) <> rm
 
