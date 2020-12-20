2d3da380ce42b80c7599380b5b6f3759bd5636e0
Dustin Sallings <dustin@spy.net>
2020-12-19 19:19:18 -0800

    Test a single failing case with fancier parsers.

--- test/Day19Tests.hs
+++ test/Day19Tests.hs
@@ -16,8 +16,12 @@ testPart1 = assertEqual "" 299 . part1 =<< getInput "input/day19"
 testPart2 :: Assertion
 testPart2 = assertEqual "" 414 . part2 =<< getInput "input/day19"
 
+testPart2Small :: Assertion
+testPart2Small = assertEqual "" 1 . part2 =<< getInput "input/day19.small"
+
 tests :: [TestTree]
 tests = [
   testCase "part1" testPart1,
+  testCase "part2 small" testPart2Small,
   testCase "part2" testPart2
   ]
