b5253489a37e270f424a376122dac5f6b829198a
Dustin Sallings <dustin@spy.net>
2020-12-19 18:04:34 -0800

    I don't need Text since I'm using a String parser

--- src/Day19.hs
+++ src/Day19.hs
@@ -7,7 +7,6 @@ import           Data.Functor                 (($>))
 import           Data.List                    (foldl1')
 import           Data.Map.Strict              (Map)
 import qualified Data.Map.Strict              as Map
-import           Data.Text                    (Text, pack, unpack)
 import           Text.Megaparsec              (anySingle, endBy, many, satisfy, sepBy, some)
 import           Text.Megaparsec.Char         (hspace, space)
 import qualified Text.Megaparsec.Char.Lexer   as L
@@ -32,11 +31,11 @@ parseRule = liftA2 (,) (L.decimal <* lexeme ":") (parseChar <|> parseSeq)
     nums = many (lexeme L.decimal)
     lexeme = L.lexeme hspace
 
-parseInput :: Parser (RuleMap, [Text])
+parseInput :: Parser (RuleMap, [String])
 parseInput = do
   rs <- Map.fromList <$> L.lexeme space (parseRule `endBy` "\n")
   ss <- (some (satisfy (`elem` ['a', 'b'])) `endBy` "\n")
-  pure (rs, pack <$> ss)
+  pure (rs, ss)
 
 mkParser :: RuleMap -> Rule -> R.ReadP ()
 mkParser rm = go
@@ -45,18 +44,17 @@ mkParser rm = go
     go (Rule ls)   = asum (p1 <$> ls)
     p1 ls = foldl1' (*>) (go . (rm Map.!) <$> ls)
 
-getInput :: FilePath -> IO (RuleMap, [Text])
+getInput :: FilePath -> IO (RuleMap, [String])
 getInput = parseFile parseInput
 
-part1 :: (RuleMap, [Text]) -> Int
+part1 :: (RuleMap, [String]) -> Int
 part1 (rm, ins) = countIf rp ins
   where
     p' = mkParser rm (rm Map.! 0) *> R.eof
-    rp x = (not.null) $ R.readP_to_S p' (unpack x)
+    rp = not . null . R.readP_to_S p'
 
-part2 :: (RuleMap, [Text]) -> Int
+part2 :: (RuleMap, [String]) -> Int
 part2 (rm, ins) = part1 (rm', ins)
   where
     rm' = (Map.fromList [(8, Rule [[42], [42, 8]]),
                          (11, Rule [[42, 31], [42, 11, 31]])]) <> rm
-
