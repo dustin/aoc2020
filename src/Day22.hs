de58fc07bc68e478e8011065433fa09afdb74b2d
Dustin Sallings <dustin@spy.net>
2020-12-22 00:41:06 -0800

    Packing keys into bits.
    It's faster without the fold, but it's a lot to write out.

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,7 +1,9 @@
 module Day22 where
 
 import           Control.Applicative        (liftA2)
-import qualified Data.Set                   as Set
+import           Data.Bits                  (shiftL, (.|.))
+import           Data.Foldable              (foldl')
+import qualified Data.IntSet                as Set
 import           Text.Megaparsec            (endBy)
 import           Text.Megaparsec.Char       (digitChar)
 import qualified Text.Megaparsec.Char.Lexer as L
@@ -26,7 +28,7 @@ play recurse = go mempty
   where
     go _ (p1, []) = (Player1, p1)
     go _ ([], p2) = (Player2, p2)
-    go s (p1:p1s, p2:p2s)
+    go s (p1deck@(p1:p1s), p2deck@(p2:p2s))
       | Set.member sk s = (Player1, [])
       | recurse && length p1s >= p1 && length p2s >= p2 =
           case go mempty ((take p1 p1s), (take p2 p2s)) of
@@ -39,7 +41,7 @@ play recurse = go mempty
       where awin = (p1s <> [p1,p2], p2s)
             bwin = (p1s, p2s <> [p2,p1])
             s' = Set.insert sk s
-            sk = (p1, last (p1:p1s), p2, last (p2:p2s))
+            sk = foldl' (\o -> (.|. shiftL o 8)) 0 ([length, head, last] <*> [p1deck,p2deck])
 
 score :: [Int] -> Int
 score = sum . zipWith (*) [1..] . reverse
