3422c576a7057c3c7a5212749579172ecdd81c57
Dustin Sallings <dustin@spy.net>
2020-12-23 23:07:43 -0800

    Various code cleanup.

--- src/Day24.hs
+++ src/Day24.hs
@@ -1,12 +1,11 @@
 module Day24 where
 
-import           Control.Applicative ((<|>))
-
-import           Control.DeepSeq     (NFData (..), rwhnf)
-import           Data.Map.Strict     (Map)
-import qualified Data.Map.Strict     as Map
-import           Data.Set            (Set)
-import           Text.Megaparsec     (endBy, many, try)
+import           Control.DeepSeq (NFData (..), rwhnf)
+import           Data.Foldable   (asum)
+import           Data.Functor    (($>))
+import qualified Data.Map.Strict as Map
+import           Data.Set        (Set)
+import           Text.Megaparsec (endBy, many)
 
 import           Advent.AoC
 import           Advent.TwoD
@@ -18,6 +17,7 @@ instance NFData Direction where rnf = rwhnf
 
 type Input = [[Direction]]
 
+-- Axial hex coordinates.
 move :: Direction -> Point -> Point
 move East (x,y)      = (x+1, y)
 move SouthEast (x,y) = (x, y+1)
@@ -30,39 +30,21 @@ around6 :: Point -> [Point]
 around6 p = [move d p | d <- [minBound .. maxBound]]
 
 parseDirection :: Parser Direction
-parseDirection = (East <$ "e")
-                 <|> (SouthEast <$ try "se")
-                 <|> (SouthWest <$ "sw")
-                 <|> (West <$ "w")
-                 <|> (NorthWest <$ try "nw")
-                 <|> (NorthEast <$ "ne")
-
-parseLine :: Parser [Direction]
-parseLine = many parseDirection
+parseDirection = asum ["e"  $> East, "se" $> SouthEast, "ne" $> NorthEast,
+                       "w"  $> West, "sw" $> SouthWest, "nw" $> NorthWest]
 
 parseInput :: Parser Input
-parseInput = parseLine `endBy` "\n"
+parseInput = many parseDirection `endBy` "\n"
 
 getInput :: FilePath -> IO Input
 getInput = parseFile parseInput
 
-findTile :: Point -> [Direction] -> Point
-findTile p []     = p
-findTile p (d:ds) = findTile (move d p) ds
-
-data Color = Black | White deriving (Show, Eq)
-
-countTiles :: Input -> Map Point Int
-countTiles inp = Map.fromListWith (+) [(point,1) | point <- points]
-  where points = findTile (0,0) <$> inp
-
-flipTiles :: Input -> Map Point Int
-flipTiles = Map.filter odd . countTiles
+blackTiles :: Input -> Set Point
+blackTiles inp = Map.keysSet . Map.filter odd . Map.fromListWith (+) $ [(point,1::Int) | point <- points]
+  where points = foldr move (0,0) <$> inp
 
 part1 :: Input -> Int
-part1 = length . flipTiles
-
-type Game = Set Point
+part1 = length . blackTiles
 
 play :: Ord k => (k -> [k]) -> Set k -> Set k
 play ex w = keep <> new
@@ -72,5 +54,4 @@ play ex w = keep <> new
     new = Map.keysSet . Map.filter (== 2) $ counts `Map.withoutKeys` w
 
 part2 :: Input -> Int
-part2 inp = length (ntimes 100 (play around6) g)
-  where g = Map.keysSet . fmap (const Black) . Map.filter odd . countTiles $ inp
+part2 = length . ntimes 100 (play around6) . blackTiles
